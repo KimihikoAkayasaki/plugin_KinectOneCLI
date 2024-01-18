4c8d8e7935d6a8853d1e39dac57b87e0764a4468
公彦赤屋先 <kimihikoakaya@gmail.com>
2024-01-19 04:25:20 +0900

    Added everything

---
+++ plugin_KinectOneCLI/KinectOneCLI.cs
@@ -0,0 +1,142 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Numerics;
+using Amethyst.Plugins.Contract;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace plugin_KinectOneCLI;
+
+[Export(typeof(ITrackingDevice))]
+[ExportMetadata("Name", "Xbox One Kinect (CLI)")]
+[ExportMetadata("Guid", "K2VRTEAM-AME2-APII-DVCE-DVCEKINV2CLI")]
+[ExportMetadata("Publisher", "公彦赤屋先")]
+[ExportMetadata("Version", "1.0.0.0")]
+[ExportMetadata("Website", "https://github.com/KimihikoAkayasaki/plugin_KinectOneCLI")]
+[ExportMetadata("DependencyLink", "https://docs.k2vr.tech/{0}/one/setup/")]
+[ExportMetadata("DependencySource",
+    "https://download.microsoft.com/download/A/7/4/A74239EB-22C2-45A1-996C-2F8E564B28ED/KinectRuntime-v2.0_1409-Setup.exe")]
+[ExportMetadata("DependencyInstaller", typeof(RuntimeInstaller))]
+[ExportMetadata("CoreSetupData", typeof(SetupData))]
+public class KinectOneCli : KinectHandler.KinectHandler, ITrackingDevice
+{
+    [Import(typeof(IAmethystHost))] private IAmethystHost Host { get; set; }
+
+    private bool PluginLoaded { get; set; }
+    public bool IsPositionFilterBlockingEnabled => false;
+    public bool IsPhysicsOverrideEnabled => false;
+    public bool IsSelfUpdateEnabled => false;
+    public bool IsFlipSupported => true;
+    public bool IsAppOrientationSupported => true;
+    public object SettingsInterfaceRoot => null;
+
+    public ObservableCollection<TrackedJoint> TrackedJoints { get; } =
+        // Prepend all supported joints to the joints list
+        new(Enum.GetValues<TrackedJointType>()
+            .Where(x => x is not TrackedJointType.JointManual)
+            .Select(x => new TrackedJoint { Name = x.ToString(), Role = x }));
+
+    public string DeviceStatusString => PluginLoaded
+        ? DeviceStatus switch
+        {
+            0 => Host.RequestLocalizedString("/Plugins/KinectOne/Statuses/Success"),
+            1 => Host.RequestLocalizedString("/Plugins/KinectOne/Statuses/NotAvailable"),
+            _ => $"Undefined: {DeviceStatus}\nE_UNDEFINED\nSomething weird has happened, though we can't tell what."
+        }
+        : $"Undefined: {DeviceStatus}\nE_UNDEFINED\nSomething weird has happened, though we can't tell what.";
+
+    public Uri ErrorDocsUri => new($"https://docs.k2vr.tech/{Host?.DocsLanguageCode ?? "en"}/one/troubleshooting/");
+
+    public void OnLoad()
+    {
+        PluginLoaded = true;
+    }
+
+    public void Initialize()
+    {
+        switch (InitializeKinect())
+        {
+            case 0:
+                Host.Log($"Tried to initialize the Kinect sensor with status: {DeviceStatusString}");
+                break;
+            case 1:
+                Host.Log($"Couldn't initialize the Kinect sensor! Status: {DeviceStatusString}", LogSeverity.Warning);
+                break;
+            default:
+                Host.Log("Tried to initialize the Kinect, but a native exception occurred!", LogSeverity.Error);
+                break;
+        }
+    }
+
+    public void Shutdown()
+    {
+        switch (ShutdownKinect())
+        {
+            case 0:
+                Host.Log($"Tried to shutdown the Kinect sensor with status: {DeviceStatusString}");
+                break;
+            case 1:
+                Host.Log($"Kinect sensor is already shut down! Status: {DeviceStatusString}", LogSeverity.Warning);
+                break;
+            case -2:
+                Host.Log("Tried to shutdown the Kinect sensor, but a SEH exception occurred!", LogSeverity.Error);
+                break;
+            default:
+                Host.Log("Tried to shutdown the Kinect sensor, but a native exception occurred!", LogSeverity.Error);
+                break;
+        }
+    }
+
+    public void Update()
+    {
+        var trackedJoints = GetTrackedKinectJoints();
+        trackedJoints.ForEach(x =>
+        {
+            TrackedJoints[trackedJoints.IndexOf(x)].TrackingState =
+                (TrackedJointState)x.TrackingState;
+
+            TrackedJoints[trackedJoints.IndexOf(x)].Position = x.Position.Safe();
+            TrackedJoints[trackedJoints.IndexOf(x)].Orientation = x.Orientation.Safe();
+        });
+    }
+
+    public void SignalJoint(int jointId)
+    {
+        // ignored
+    }
+
+    public override void StatusChangedHandler()
+    {
+        // The Kinect sensor requested a refresh
+        InitializeKinect();
+
+        // Request a refresh of the status UI
+        Host?.RefreshStatusInterface();
+    }
+}
+
+internal static class PoseUtils
+{
+    public static Quaternion Safe(this Quaternion q)
+    {
+        return (q.X is 0 && q.Y is 0 && q.Z is 0 && q.W is 0) ||
+               float.IsNaN(q.X) || float.IsNaN(q.Y) || float.IsNaN(q.Z) || float.IsNaN(q.W)
+            ? Quaternion.Identity // Return a placeholder quaternion
+            : q; // If everything is fine, return the actual orientation
+    }
+
+    public static Vector3 Safe(this Vector3 v)
+    {
+        return float.IsNaN(v.X) || float.IsNaN(v.Y) || float.IsNaN(v.Z)
+            ? Vector3.Zero // Return a placeholder position vector
+            : v; // If everything is fine, return the actual orientation
+    }
+}
---
+++ plugin_KinectOneCLI/RuntimeInstaller.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Amethyst.Plugins.Contract;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media;
+using RestSharp;
+
+namespace plugin_KinectOneCLI;
+
+internal class SetupData : ICoreSetupData
+{
+    public object PluginIcon => new PathIcon
+    {
+        Data = (Geometry)XamlBindingHelper.ConvertValue(typeof(Geometry),
+            "M45.26,18.3V15.93H69.51V1.1H0V16H24.25v2.37H0v5.25H69.51V18.3ZM9.36,13.19A4.63,4.63,0,0,1,4.65,8.45a4.61,4.61,0,0,1,4.6-4.67,4.71,4.71,0,1,1,.11,9.41Z")
+    };
+
+    public string GroupName => "kinect";
+    public Type PluginType => typeof(ITrackingDevice);
+}
+
+internal class RuntimeInstaller : IDependencyInstaller
+{
+    public IDependencyInstaller.ILocalizationHost Host { get; set; }
+
+    public List<IDependency> ListDependencies()
+    {
+        return new List<IDependency>
+        {
+            new KinectRuntime
+            {
+                Host = Host,
+                Name = Host?.RequestLocalizedString("/Plugins/KinectOne/Dependencies/Runtime/Name") ??
+                       "Kinect for Xbox One Runtime"
+            }
+        };
+    }
+}
+
+internal class KinectRuntime : IDependency
+{
+    public IDependencyInstaller.ILocalizationHost Host { get; set; }
+
+    public string Name { get; set; }
+    public bool IsMandatory => true;
+
+    public bool IsInstalled
+    {
+        get
+        {
+            try
+            {
+                // Well, this is pretty much all we need for the plugin to be loaded
+                return File.Exists(
+                    @"C:\Windows\Microsoft.NET\assembly\GAC_64\Microsoft.Kinect\v4.0_2.0.0.0__31bf3856ad364e35\Microsoft.Kinect.dll");
+            }
+            catch (Exception)
+            {
+                // Access denied?
+                return false;
+            }
+        }
+    }
+
+    public string InstallerEula
+    {
+        get
+        {
+            try
+            {
+                return File.ReadAllText(Path.Join(
+                    Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.FullName,
+                    "Assets", "Resources", "eula.md"));
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    public async Task<bool> Install(IProgress<InstallationProgress> progress, CancellationToken cancellationToken)
+    {
+        // Amethyst will handle this exception for us anyway
+        cancellationToken.ThrowIfCancellationRequested();
+        var paths = new[]
+        {
+            Path.Join(Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.FullName,
+                "Assets", "Resources", "Dependencies", "KinectRuntime-x64.msi")
+        };
+
+        // Copy to temp if amethyst is packaged
+        // ReSharper disable once InvertIf
+        if (PackageUtils.IsAmethystPackaged)
+        {
+            // Create a shared folder with the dependencies
+            var dependenciesFolder = await ApplicationData.Current.TemporaryFolder.CreateFolderAsync(
+                Guid.NewGuid().ToString().ToUpper(), CreationCollisionOption.OpenIfExists);
+
+            // Copy all driver files to Amethyst's local data folder
+            new DirectoryInfo(Path.Join(Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.FullName,
+                    "Assets", "Resources", "Dependencies"))
+                .CopyToFolder(dependenciesFolder.Path);
+
+            // Update the installation paths
+            paths = new[]
+            {
+                Path.Join(dependenciesFolder.Path, "KinectRuntime-x64.msi")
+            };
+        }
+
+        // Finally install the packages
+        return InstallFiles(paths, progress, cancellationToken);
+    }
+
+    private bool InstallFiles(IEnumerable<string> files,
+        IProgress<InstallationProgress> progress, CancellationToken cancellationToken)
+    {
+        // Amethyst will handle this exception for us anyway
+        cancellationToken.ThrowIfCancellationRequested();
+
+        // Execute each install
+        foreach (var installFile in files)
+            try
+            {
+                // msi /qn /norestart
+                progress.Report(new InstallationProgress
+                {
+                    IsIndeterminate = true,
+                    StageTitle =
+                        (Host?.RequestLocalizedString("/Plugins/KinectOne/Stages/Installing") ??
+                         "Installing {0}...").Replace("{0}", Path.GetFileName(installFile))
+                });
+
+                var msiExecutableStart = new ProcessStartInfo
+                {
+                    FileName = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Windows),
+                        @"System32\msiexec.exe"),
+                    WorkingDirectory = Directory.GetParent(installFile)!.FullName,
+                    Arguments = $"/i {installFile} /quiet /qn /norestart ALLUSERS=1",
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                var msiExecutable = Process.Start(msiExecutableStart);
+                msiExecutable!.WaitForExit(60000);
+            }
+            catch (Exception e)
+            {
+                progress.Report(new InstallationProgress
+                {
+                    IsIndeterminate = true,
+                    StageTitle =
+                        (Host?.RequestLocalizedString("/Plugins/KinectOne/Stages/Exceptions/Other") ??
+                         "Exception: {0}").Replace("{0}", e.Message)
+                });
+
+                return false;
+            }
+
+        return true;
+    }
+}
+
+public static class RestExtensions
+{
+    public static Task<byte[]> ExecuteDownloadDataAsync(this RestClient client, string baseUrl, RestRequest request)
+    {
+        client.Options.BaseUrl = new Uri(baseUrl);
+        return client.DownloadDataAsync(request);
+    }
+
+    public static Task<Stream> ExecuteDownloadStreamAsync(this RestClient client, string baseUrl, RestRequest request)
+    {
+        client.Options.BaseUrl = new Uri(baseUrl);
+        return client.DownloadStreamAsync(request);
+    }
+}
+
+public static class StreamExtensions
+{
+    public static async Task CopyToWithProgressAsync(this Stream source,
+        Stream destination, CancellationToken cancellationToken,
+        Action<long> progress = null, int bufferSize = 10240)
+    {
+        var buffer = new byte[bufferSize];
+        var total = 0L;
+        int amtRead;
+
+        do
+        {
+            amtRead = 0;
+            while (amtRead < bufferSize)
+            {
+                var numBytes = await source.ReadAsync(
+                    buffer, amtRead, bufferSize - amtRead, cancellationToken);
+                if (numBytes == 0) break;
+                amtRead += numBytes;
+            }
+
+            total += amtRead;
+            await destination.WriteAsync(buffer, 0, amtRead, cancellationToken);
+            progress?.Invoke(total);
+        } while (amtRead == bufferSize);
+    }
+}
